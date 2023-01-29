67e6a86629cae35c3d7177b54772a83930b3ee3a
Botwhatsapp <114358388+Botwhatsapp@users.noreply.github.com>
2023-01-30 00:09:49 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['9660540637980'] //ur owner number
-global.ownername = "ABDUL RAHMAN🐼🍷 ٫" //ur owner name
-global.ytname = "YT: ABDUL RAHMAN" //ur yt chanel name
-global.socialm = "GitHub: ABDUL RAHMAN" //ur github or insta name
-global.location = "Egypt, Cairo, Aizawl" //ur location
+global.vcardowner = ['01063398639'] //ur owner number
+global.ownername = "🌀KU👑RA💥MA🌀 ٫" //ur owner name
+global.ytname = "YT: . " //ur yt chanel name
+global.socialm = "GitHub: ZORAKIMORINKASHI" //ur github or insta name
+global.location = "Egypt, alexandria, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['9660540637980']
-global.ownertag = '9660540637980' //ur tag number
-global.botname = 'BOT ABDUL RAHMAN🐼🍷 ٫' //ur bot name
-global.linkz = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //your theme url which will be displayed on whatsapp
-global.websitex = "https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv" //ur website to be displayed
-global.botscript = 'https://chat.whatsapp.com/LXZnV3rPFqD4ryPvG0Cyvvv' //script link
-global.reactmoji = "🌚" //ur menu react emoji
-global.themeemoji = "🌞" //ur theme emoji
-global.packname = "⍣عبدالرحمن⍣" //ur sticker watermark packname
-global.author = "⍣9660540637980⍣" //ur sticker watermark author
+global.owner = ['01063398639']
+global.ownertag = '01063398639' //ur tag number
+global.botname = 'kurama.bot ٫' //ur bot name
+global.linkz = " " //your theme url which will be displayed on whatsapp
+global.websitex = " " //ur website to be displayed
+global.botscript = 'https://chat.whatsapp.com/G6ilYtowqQZ7okVSqSgKpd' //script link
+global.reactmoji = "🌀" //ur menu react emoji
+global.themeemoji = "💥" //ur theme emoji
+global.packname = "MADE BY KURAMA⍣" //ur sticker watermark packname
+global.author = "⍣01063398639⍣" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
